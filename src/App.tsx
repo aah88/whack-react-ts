cdd4a7b50397dcba378d82c13b703b7bf53c8c67
Ahmad Hamadeh <ahmad.hamadeh@itgain.de>
2025-11-13 23:09:38 +0100

    Maulwurf

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+
+import './App.css'
+import Aufgabe from './components/aufgabe'
+
+ 
+
+function App() {
+
+
+
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+        <Aufgabe/>
+    </>
+  )
+}
+
+export default App
